944218c16d3694eb2bbfbee57aa09c0927429bf3
Nathaniel Titterton <nate@berkeley.edu>
2013-05-13 14:42:48 -0700

    add whole text file loggin functionality (back end)

--- logger-plugin/src/edu/berkeley/eduride/loggerplugin/loggers/AbstractLogger.java
+++ logger-plugin/src/edu/berkeley/eduride/loggerplugin/loggers/AbstractLogger.java
@@ -1,46 +1,58 @@
 package edu.berkeley.eduride.loggerplugin.loggers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Calendar;
+
+import util.Base64Coder;
+import util.FileAsString;
 
 import edu.berkeley.eduride.loggerplugin.EduRideLogger;
 import edu.berkeley.eduride.loggerplugin.LoggerInstaller;
 
-
-
 public abstract class AbstractLogger {
 
-	
 	static String defaultType = "baseLogger";
-	protected String myType = ""; 
-	
+	protected String myType = "";
+
 	public AbstractLogger() {
 		myType = this.getClass().getSimpleName();
 		LoggerInstaller.trackLogger(this);
 		log("loggerInstall", myType + " instantiated standard.");
 	}
-	
+
 	public AbstractLogger(String name) {
 		myType = name;
 		LoggerInstaller.trackLogger(this);
 		log("loggerInstaller", myType + " instantiated.");
 	}
-	
-	
+
 	// Note -- logging should move out to LogEvents
-	
-	public void log(String content){
+
+	public void log(String content) {
 		log(myType, content);
 	}
-	
+
+	public void log(File txtfile) throws FileNotFoundException {
+		log(myType, txtfile);
+	}
+
 	public void log(String action, String content) {
 		EduRideLogger.log(action, content);
-		System.out.println("(" + System.currentTimeMillis() + ") LOGGER " + action + ": " + content);
+		System.out.println("(" + System.currentTimeMillis() + ") LOGGER "
+				+ action + ": " + content);
 	}
-	
-	
+
 	public static void logStatic(String action, String content) {
 		EduRideLogger.log(action, content);
-		System.out.println("(" + System.currentTimeMillis() + ") LOGGER " + action + ": " + content);
+		System.out.println("(" + System.currentTimeMillis() + ") LOGGER "
+				+ action + ": " + content);
+	}
+
+	public void log(String action, File txtfile) throws FileNotFoundException {
+		String fStr = FileAsString.convert(txtfile);
+		char[] charr = Base64Coder.encode(fStr.getBytes());
+		log(action, charr.toString());
 	}
 
 }
---
+++ logger-plugin/src/util/FileAsString.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class FileAsString {
+
+	public static String convert(File txtfile) throws FileNotFoundException {
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					new FileInputStream(txtfile), "UTF8"));
+			String tmp;
+			StringBuilder out = new StringBuilder();
+
+			while ((tmp = in.readLine()) != null) {
+				out.append(tmp);
+			}
+
+			return out.toString();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
